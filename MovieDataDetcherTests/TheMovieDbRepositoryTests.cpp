d06ab4f82ff8c39352bda24768d5a09fcbacf500
Angel Vargas <vargas.angel.112@gmail.com>
2019-09-22 18:06:05 -0500

    5 Selecting Between Behaviors

--- MovieDataDetcherTests/TheMovieDbRepositoryTests.cpp
+++ MovieDataDetcherTests/TheMovieDbRepositoryTests.cpp
@@ -6,6 +6,7 @@
 #include "FakeRestApiClient.h"
 
 using namespace testing;
+const std::string fictiveApiKey = "1234";
 
 TEST(TheMovieDbRepositoryTests, FindMovieData_ApiReturnEmptyString_ThrowException)
 {
@@ -13,7 +14,7 @@ TEST(TheMovieDbRepositoryTests, FindMovieData_ApiReturnEmptyString_ThrowExceptio
 	FakeRestApiClient fakeClient;
 	EXPECT_CALL(fakeClient, HttpGet(_)).WillOnce(Return(""));
 
-	TheMovieDbRepository repository("api-key", factory, fakeClient);
+	TheMovieDbRepository repository(fictiveApiKey, factory, fakeClient);
 
 	ASSERT_THROW(
 		repository.FindMovieData("movie name"), MovieNotFoundException);
@@ -25,8 +26,27 @@ TEST(TheMovieDbRepositoryTests, FindMovieData_ApiReturnsStringWithoutMovie_Throw
 	FakeRestApiClient fakeClient;
 	EXPECT_CALL(fakeClient, HttpGet(_)).WillOnce(Return("{\"results\":[]}"));
 
-	TheMovieDbRepository repository("api-key", factory, fakeClient);
+	TheMovieDbRepository repository(fictiveApiKey, factory, fakeClient);
 
 	ASSERT_THROW(
 		repository.FindMovieData("movie name"), MovieNotFoundException);
+}
+
+TEST(TheMovieDbRepositoryTests, FindMovieData_ApiReturnsValidData_ReturnMovieResult)
+{
+	const char* movieList = "{\"page\":1,\"results\":[{\"id\":123,\"title\":\"my movie title\"}],\"total_results\":1,\"total_pages\":1}";
+	const char* movieMeta = "{\"imdb_id\":\"myId\",\"title\":\"my movie title\",\"overview\":\"this is a plot\",\"runtime\":180}";
+
+	std::string firstQuery = "http://api.themoviedb.org/3/search/movie?api_key=1234&page=1&query=my%2fmovie%2ftitle";
+	std::string secondQuery = "http://api.themoviedb.org/3/movie/123?api_key=1234";
+	TheMovieDbDataFactory factory;
+	FakeRestApiClient fakeClient;
+
+	EXPECT_CALL(fakeClient, HttpGet(_)).WillOnce(Return(movieList)).WillOnce(Return(movieMeta));
+
+	TheMovieDbRepository repository(fictiveApiKey, factory, fakeClient);
+
+	auto result = repository.FindMovieData("my movie title");
+
+	EXPECT_EQ(result->GetImdbId(), "myId");
 }
