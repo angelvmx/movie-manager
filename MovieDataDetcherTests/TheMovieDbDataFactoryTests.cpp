f8bbe24af23b0bb0351c4b21d5a74328dc3e4bf2
Angel Vargas <vargas.angel.112@gmail.com>
2019-10-03 06:43:30 -0500

    7 Demo: Creating New Matchers

--- MovieDataDetcherTests/TheMovieDbDataFactoryTests.cpp
+++ MovieDataDetcherTests/TheMovieDbDataFactoryTests.cpp
@@ -16,6 +16,18 @@ TEST(TheMovieDbDataFactoryTests, CreateFromJson_PassEmptyString_ReturnEmptyClass
     EXPECT_EQ(result->GetPlot(), "");
 }
 
+::std::ostream& operator<<(::std::ostream& os, const MovieData& movieData) {
+return os << "Id: " << movieData.GetImdbId() << " Title: " << movieData.GetTitle() << " Plot: " << movieData.GetPlot() << " Length: " << movieData.GetLengthMin();
+}
+
+MATCHER_P4(MovieDataEq, expectedId, expectedTitle, expectedPlot, expectedLength, "Title: " + std::string(negation ? "isn't": "is") + "Equal to: " + expectedTitle)
+{
+     return arg.GetImdbId() == expectedId &&
+        arg.GetTitle() == expectedTitle &&
+        arg.GetPlot() == expectedPlot &&
+        arg.GetLengthMin() == expectedLength;
+}
+
 TEST(TheMovieDbDataFactoryTests_CreateFromJson_PassValidString_ReturnMovieDetails)
 {
     std::string input = "{\"id\":284052,\"imdb_id\":\"tt1211837\",\"overview\":\"The movie's plot\",\"title\":\"a movie\",\"runtime\":115}";
@@ -24,10 +36,7 @@ TEST(TheMovieDbDataFactoryTests_CreateFromJson_PassValidString_ReturnMovieDetail
 
     auto result = factory.CreateFromJson(input);
 
-    EXPECT_EQ(result->GetImdbId(), "tt1211837");
-    EXPECT_EQ(result->GetTitle(), "a movie");
-    EXPECT_EQ(result->GetPlot(), "The movie's plot");
-    EXPECT_EQ(result->GetLengthMin(), 115);
+	ASSERT_THAT(*result, MovieDataEq("tt1211837", "a wrong movie", "The movie's plot", 115));
 }
 
 TEST(TheMovieDbDataFactoryTests, GetActorList_PassValiedStringWithOneActor_ReturnActor)
